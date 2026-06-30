49572bfd1fea5980b6ffe261105a1338dec953a5
Kyle McColgan <kdmbus21@gmail.com>
2026-06-30 00:10:53 -0500

    vite.config.js: Replace Lightning CSS with esbuild, and address a missing closing curly brace in TimerControls.

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 //File name: vite.config.js
 //Author: Kyle McColgan
-//Date: 16 June 2026
+//Date: 30 June 2026
 //Description: This file contains the Vite configuration for the timer React project.
 
 import { defineConfig } from 'vite'
@@ -12,6 +12,13 @@ export default defineConfig({
   plugins: [react()],
   define: {
     'process.env': {}, // Prevents SSR-related undefined issues
+  },
+  css: {
+    transformer: 'postcss',
+    minify: 'esbuild',
+  },
+  build: {
+    cssMinify: 'esbuild'
   },
   test: {
     globals: true,
